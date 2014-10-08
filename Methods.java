bbcd98dd9d087d1afb363c2469731c8a5b68eaf8
codeninjathe <sponge7226@gmail.com>
2014-10-07 20:01:14 -0500

    Create Methods.java

---
+++ Methods.java
@@ -0,0 +1,18 @@
+public class Methods{
+  public static void main(String[] args){
+    String s = ""; //Put any value there
+    while(!onlyNumsDouble(s)){
+      s = ""; //get a new value for s
+    }
+  }
+  public static void onlyNumsDouble(String s){
+    s = s.trim();
+    for(int w = 0; w < 10; w++){
+      s = s.replaceAll(w + "", "");
+    }
+    s = s.replace(".", "");
+    if(s.length() >= 1)
+      return false;
+    return true;
+  }
+}
